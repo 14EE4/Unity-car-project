bf199ccaa816f9ce4c71a459188eb03dc350a5c5
14EE4 <pyeong85732@naver.com>
2026-05-17 17:12:20 +0900

    feat: Improve logging and texture handling in MenuBackgroundRawImage for better debugging and performance

--- Assets/Script/UI/MenuBackgroundRawImage.cs
+++ Assets/Script/UI/MenuBackgroundRawImage.cs
@@ -10,37 +10,47 @@ public class MenuBackgroundRawImage : MonoBehaviour
     {
         rawImage = GetComponent<RawImage>();
         rawImage.enabled = false;
+        transform.SetAsFirstSibling();
+        Debug.Log("MenuBackgroundRawImage: Awake - rawImage found");
     }
 
     void OnEnable()
     {
+        transform.SetAsFirstSibling();
+        Debug.Log($"MenuBackgroundRawImage: OnEnable - BackgroundTexture is {(PersistentGameCamera.BackgroundTexture!=null ? "present" : "null")}");
         if (PersistentGameCamera.BackgroundTexture != null)
         {
             rawImage.texture = PersistentGameCamera.BackgroundTexture;
             rawImage.enabled = true;
+            Debug.Log($"MenuBackgroundRawImage: OnEnable - assigned texture {PersistentGameCamera.BackgroundTexture.width}x{PersistentGameCamera.BackgroundTexture.height}");
         }
         else
         {
             PersistentGameCamera.OnTextureReady += OnTextureReady;
+            Debug.Log("MenuBackgroundRawImage: OnEnable - subscribed to OnTextureReady");
         }
     }
 
     void OnDisable()
     {
         PersistentGameCamera.OnTextureReady -= OnTextureReady;
+        Debug.Log("MenuBackgroundRawImage: OnDisable - unsubscribed");
     }
 
     void OnTextureReady(RenderTexture rt)
     {
+        transform.SetAsFirstSibling();
         if (rt != null)
         {
             rawImage.texture = rt;
             rawImage.enabled = true;
+            Debug.Log($"MenuBackgroundRawImage: OnTextureReady - texture ready {rt.width}x{rt.height}");
         }
         else
         {
             rawImage.texture = null;
             rawImage.enabled = false;
+            Debug.Log("MenuBackgroundRawImage: OnTextureReady - texture null, disabled");
         }
     }
 }
