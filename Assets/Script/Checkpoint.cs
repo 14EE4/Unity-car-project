cede0e9fbed5f9c1402b3617adf69f616da0fed6
14ee4 <pyeong85732@naver.com>
2026-05-23 13:59:16 +0900

    Refactor Checkpoint class to remove transparency management and related properties, simplifying the code structure

--- Assets/Script/Checkpoint.cs
+++ Assets/Script/Checkpoint.cs
@@ -3,29 +3,10 @@ using UnityEngine;
 public class Checkpoint : MonoBehaviour
 {
     public bool IsVisited { get; private set; } = false;
-
-    private Renderer[] checkpointRenderers;
-    private Collider[] checkpointColliders;
-    private Color[][] originalColors;
     private CheckpointManager checkpointManager;
 
     private void Awake()
     {
-        checkpointRenderers = GetComponentsInChildren<Renderer>(true);
-        checkpointColliders = GetComponentsInChildren<Collider>(true);
-        originalColors = new Color[checkpointRenderers.Length][];
-
-        for (int i = 0; i < checkpointRenderers.Length; i++)
-        {
-            Material[] materials = checkpointRenderers[i].materials;
-            originalColors[i] = new Color[materials.Length];
-
-            for (int j = 0; j < materials.Length; j++)
-            {
-                originalColors[i][j] = materials[j].color;
-            }
-        }
-
         checkpointManager = FindObjectOfType<CheckpointManager>();
         if (checkpointManager == null)
         {
@@ -42,7 +23,7 @@ public class Checkpoint : MonoBehaviour
                 IsVisited = true;
                 Debug.Log($"Checkpoint {gameObject.name} validated and visited.");
 
-                SetCheckpointTransparency(0.15f);
+                gameObject.SetActive(false);
             }
             else
             {
@@ -54,34 +35,6 @@ public class Checkpoint : MonoBehaviour
     public void ResetCheckpoint()
     {
         IsVisited = false;
-        SetCheckpointTransparency(1f);
-    }
-
-    private void SetCheckpointTransparency(float alpha)
-    {
-        for (int i = 0; i < checkpointRenderers.Length; i++)
-        {
-            Renderer checkpointRenderer = checkpointRenderers[i];
-            if (checkpointRenderer != null)
-            {
-                checkpointRenderer.enabled = true;
-
-                Material[] materials = checkpointRenderer.materials;
-                for (int j = 0; j < materials.Length; j++)
-                {
-                    Color color = originalColors[i][j];
-                    color.a = alpha;
-                    materials[j].color = color;
-                }
-            }
-        }
-
-        foreach (var checkpointCollider in checkpointColliders)
-        {
-            if (checkpointCollider != null)
-            {
-                checkpointCollider.enabled = alpha >= 1f;
-            }
-        }
+        gameObject.SetActive(true);
     }
 }
